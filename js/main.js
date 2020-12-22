6ce37923c9633af5bb83d71b717021b4e5c72d1e
Tklao <carlosmelo1295@gmail.com>
2020-12-22 14:53:54 -0300

    Scroll Animation 🚀

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
-setTimeout(() => {
-  const fistArticle = document.querySelector(".sobre");
-  fistArticle.style.transform = "translate(0px)";
-  fistArticle.style.opacity = "1";
-  fistArticle.style.transition = "1s";
-}, 1000);
+"use strict"
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+
+const getDataAnime = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  getDataAnime.forEach(function (elemento) {
+    if (windowTop > elemento.offsetTop) {
+      elemento.classList.add("animate");
+    } else {
+      elemento.classList.remove("animate");
+    }
+  });
+};
+animeScroll();
+
+
+if (getDataAnime.length) {
+  window.addEventListener("scroll", debounce(() => {
+    animeScroll();
+  }, 100))
+}
+
+
+
+//--------------------------------------------------
 
 const iconGit = document
   .querySelector(".icon-tabler-brand-github")
